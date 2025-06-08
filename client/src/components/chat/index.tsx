06caec4793172580ff937b0e68bbda126a1e8af7
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-06-08 23:31:51 +0530

    feat(video): draggable div layout for display clients video

--- client/src/components/chat/index.tsx
+++ client/src/components/chat/index.tsx
@@ -1,3 +1,4 @@
+export { DraggableVideo } from "./draggable-video";
 export { ChatContainer } from "./chat-container";
 export { ContactsContainer } from "./contacts-container";
 export { EmptyChatContainer } from "./empty-chat-container";
