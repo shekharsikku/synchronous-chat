8687b1a0caf083559098e8e299a65bf276ca655e
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-08-10 12:03:49 +0530

    feat(peerjs): feature for sharing file peer to peer

--- client/src/components/chat/index.tsx
+++ client/src/components/chat/index.tsx
@@ -2,3 +2,4 @@ export { DraggableVideo } from "./draggable-video";
 export { ChatContainer } from "./chat-container";
 export { ContactsContainer } from "./contacts-container";
 export { EmptyChatContainer } from "./empty-chat-container";
+export { PeerShare } from "./peer-share";
