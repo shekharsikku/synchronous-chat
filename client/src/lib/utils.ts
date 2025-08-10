4849e7a8e26e2e3d7670d926295383f17023132a
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-08-10 09:53:49 +0530

    refactor(utils): utility function for format file size

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -189,6 +189,17 @@ export const isValidUrl = (url: string): boolean => {
   }
 };
 
+export function formatSize(bytes: number): string {
+  if (bytes === 0) return "0 Bytes";
+
+  const k = 1024;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + " " + sizes[i];
+}
+
 /*
 
 import CryptoJS from "crypto-js";
