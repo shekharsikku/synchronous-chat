4078f9ff32d3e56b7ccf5715cab8d0e6e7e2b869
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-08-10 10:53:39 +0530

    feat(socket.io): events for file sharing connection

--- src/socket.ts
+++ src/socket.ts
@@ -112,6 +112,22 @@ io.on("connection", (socket: Socket) => {
     });
   });
 
+  socket.on("before:share-request", ({ shareInfo }) => {
+    const socketId = getSocketId(shareInfo.to);
+
+    socket.to(socketId).emit("after:share-request", {
+      shareInfo,
+    });
+  });
+
+  socket.on("before:file-request", ({ shareInfo }) => {
+    const socketId = getSocketId(shareInfo.to);
+
+    socket.to(socketId).emit("after:file-request", {
+      shareInfo,
+    });
+  });
+
   socket.on("disconnect", () => {
     for (const [userId, sockets] of userSocketMap.entries()) {
       if (sockets.has(socket.id)) {
