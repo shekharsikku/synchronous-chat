d32663a756e6ed1bf11270c555934cf8050d7cab
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-12-13 12:52:14 +0530

    refactor(response): improve type-safe response handlers

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -2,11 +2,9 @@ import type { Response } from "express";
 
 class HttpError extends Error {
   public code: number;
-  public message: string;
 
   constructor(code: number, message: string, stack: string = "") {
     super(message);
-
     this.code = code;
     this.message = message;
     this.stack = stack;
@@ -21,17 +19,15 @@ type TypeResponse<T = any, E = any> = {
   error?: E;
 };
 
-const ErrorResponse = (res: Response, code: number, message: string, error: any = null) => {
-  const response: TypeResponse<null> = { success: false, message };
-
-  if (error) response.error = error;
+const ErrorResponse = <E>(res: Response, code: number, message: string, error?: E) => {
+  const response: TypeResponse<undefined, E> = { success: false, message };
+  if (error !== undefined) response.error = error;
   res.status(code).json(response);
 };
 
-const SuccessResponse = (res: Response, code: number, message: string, data: any = null) => {
-  const response: TypeResponse<any, null> = { success: true, message };
-
-  if (data) response.data = data;
+const SuccessResponse = <T>(res: Response, code: number, message: string, data?: T) => {
+  const response: TypeResponse<T, undefined> = { success: true, message };
+  if (data !== undefined) response.data = data;
   res.status(code).json(response);
 };
 
