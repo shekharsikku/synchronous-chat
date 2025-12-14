4d18c1708d1c337c7d8f2864ef9b53082c0bc95f
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-12-14 13:32:41 +0530

    refactor(group): move member details from member list

--- client/src/components/chat/member-list.tsx
+++ client/src/components/chat/member-list.tsx
@@ -1,7 +1,6 @@
-import { Badge } from "@/components/ui/badge";
+import { MemberDetails } from "@/components/chat/member-details";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useContacts } from "@/hooks";
-import { cn, getAvatar } from "@/lib/utils";
 import { useAuthStore } from "@/lib/zustand";
 
 interface GroupMembersProps {
@@ -11,34 +10,14 @@ interface GroupMembersProps {
 const GroupMembersList: React.FC<GroupMembersProps> = ({ selectedChatData }) => {
   const { contacts } = useContacts();
   const { userInfo } = useAuthStore();
-
-  const getMemberDetails = (member: any) => {
-    if (member === userInfo?._id) {
-      return {
-        id: userInfo?._id,
-        name: userInfo?.name,
-        image: getAvatar(userInfo),
-        bio: userInfo?.bio,
-      };
-    }
-
-    const detail = contacts?.find((contact) => contact._id === member);
-
-    return {
-      id: detail?._id,
-      name: detail?.name,
-      image: getAvatar(detail),
-      gender: detail?.gender,
-      bio: detail?.bio,
-    };
-  };
+  const adminId = selectedChatData.admin;
 
   const sortedMembers = selectedChatData?.members.sort((a: any, b: any) => {
     if (a === userInfo?._id) return -1;
     if (b === userInfo?._id) return 1;
 
-    if (a === selectedChatData.admin) return -1;
-    if (b === selectedChatData.admin) return 1;
+    if (a === adminId) return -1;
+    if (b === adminId) return 1;
 
     return 0;
   });
@@ -47,46 +26,15 @@ const GroupMembersList: React.FC<GroupMembersProps> = ({ selectedChatData }) => 
     <div className="min-h-36 max-h-60 py-1 overflow-y-scroll scrollbar-hide">
       <ScrollArea className="min-h-20 overflow-y-auto scrollbar-hide">
         <div className="flex flex-col gap-4">
-          {sortedMembers.map((current: any) => {
-            const details = getMemberDetails(current);
-            const isYou = userInfo?._id === details.id;
-            const isAdmin = selectedChatData.admin === details.id;
-
-            return (
-              <div
-                key={details.id}
-                className={cn(
-                  "w-full flex items-center gap-4 px-3 py-2 border rounded",
-                  isAdmin && isYou && "border-gray-300 dark:border-gray-700"
-                )}
-              >
-                <div className="flex-none size-max">
-                  <img
-                    src={details.image}
-                    alt={details.name}
-                    className="size-8 rounded-full object-cover border border-border"
-                  />
-                </div>
-                <div className="w-full flex justify-between items-center">
-                  <div className="flex flex-col">
-                    <h6 className="text-sm">{details.name}</h6>
-                    <p
-                      className={cn(
-                        "text-xs inline-block align-middle truncate",
-                        isAdmin && isYou ? "max-w-24 md:max-w-36" : "max-w-32 md:max-w-44 "
-                      )}
-                    >
-                      {details.bio}
-                    </p>
-                  </div>
-
-                  {(isAdmin || isYou) && (
-                    <Badge variant="secondary">{isAdmin && isYou ? "Admin • You" : isAdmin ? "Admin" : "You"}</Badge>
-                  )}
-                </div>
-              </div>
-            );
-          })}
+          {sortedMembers.map((current: any) => (
+            <MemberDetails
+              key={current?.id}
+              contacts={contacts}
+              userInfo={userInfo!}
+              memberId={current}
+              adminId={adminId}
+            />
+          ))}
         </div>
       </ScrollArea>
     </div>
