24219e99c109f7e101ddb94e54c20292bb95b2ab
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2026-04-16 11:31:12 +0530

    refactor(helpers): for throw api error & return response

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,12 +1,9 @@
 import { createSecretKey, createHash } from "node:crypto";
 import { deflateSync } from "node:zlib";
-
 import { CompactEncrypt, SignJWT } from "jose";
-
 import env from "#/utils/env.js";
-
 import type { UserInterface } from "#/interfaces/index.js";
-import type { CookieOptions, Response } from "express";
+import type { CookieOptions, NextFunction, Request, RequestHandler, Response } from "express";
 import type { Types } from "mongoose";
 
 export const generateSecret = async () => {
@@ -89,3 +86,40 @@ export const createUserInfo = (user: UserInterface) => {
 
   return userInfo as UserInterface;
 };
+
+export const asyncHandler = <P = {}, ResBody = unknown, ReqBody = unknown, ReqQuery = {}>(
+  func: RequestHandler<P, ResBody, ReqBody, ReqQuery>
+): RequestHandler<P, ResBody, ReqBody, ReqQuery> => {
+  return (req: Request<P, ResBody, ReqBody, ReqQuery>, res: Response<ResBody>, next: NextFunction) => {
+    Promise.resolve(func(req, res, next)).catch(next);
+  };
+};
+
+export class ApiError extends Error {
+  public code: number;
+
+  constructor(code: number, message: string) {
+    super(message);
+    this.code = code;
+    this.name = this.constructor.name;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+type TypeResponse<T = unknown, E = unknown> =
+  | { success: true; message: string; data?: T }
+  | { success: false; message: string; error?: E };
+
+export class ApiResponse {
+  static success = <T>(res: Response, code: number, message: string, data?: T): Response<TypeResponse<T, never>> => {
+    const response: TypeResponse<T, never> = { success: true, message };
+    if (data !== undefined) response.data = data;
+    return res.status(code).json(response);
+  };
+
+  static error = <E>(res: Response, code: number, message: string, error?: E): Response<TypeResponse<never, E>> => {
+    const response: TypeResponse<never, E> = { success: false, message };
+    if (error !== undefined) response.error = error;
+    return res.status(code).json(response);
+  };
+}
